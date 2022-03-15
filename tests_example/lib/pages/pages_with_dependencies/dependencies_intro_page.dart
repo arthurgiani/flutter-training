0dc3caa85a1bf46ee17e7cfaf65b64e27bdab9dc
arthurgiani <arthur@getleslie.com>
2022-03-15 12:34:05 -0300

    dependencies intro page

---
+++ tests_example/lib/pages/pages_with_dependencies/dependencies_intro_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tests_example/blocs/list_cubit.dart';
+import 'package:tests_example/components/custom_button.dart';
+import 'package:tests_example/pages/pages_with_dependencies/cubit_fake_list_page.dart';
+import 'package:tests_example/repositories/list_repository.dart';
+
+class DependenciesIntroPage extends StatefulWidget {
+  const DependenciesIntroPage({Key? key}) : super(key: key);
+
+  @override
+  State<DependenciesIntroPage> createState() => _DependenciesIntroPageState();
+}
+
+class _DependenciesIntroPageState extends State<DependenciesIntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dependency Intro Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CustomButton(
+              label: 'Fake List - Cubit',
+              nextWidget: BlocProvider(
+                create: (context) => ListCubit(
+                  listRepository: ListRepository(),
+                )..getStringList(),
+                child: const CubitFakeListPage(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
