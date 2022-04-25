6e0c87ef0d04e3024ecfdc5a92555af512beed40
arthurgiani <arthur@getleslie.com>
2022-04-25 11:08:12 -0300

    add full screen mode on flutter engine

--- flutter_in_native_app/ios_native_app/ios_native_app/ViewController.swift
+++ flutter_in_native_app/ios_native_app/ios_native_app/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
         let flutterEngine = (UIApplication.shared.delegate as! AppDelegate).flutterEngine
         let flutterViewController =
         FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+        flutterViewController.modalPresentationStyle = .fullScreen
         present(flutterViewController, animated: true, completion: nil)
     }
 }
