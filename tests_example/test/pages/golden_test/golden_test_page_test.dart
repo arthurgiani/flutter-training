4a7cfc99c16df813f3a5e638fb473221bfcce1c1
arthurgiani <arthur@getleslie.com>
2022-05-11 16:01:23 -0300

    change test description

--- tests_example/test/pages/golden_test/golden_test_page_test.dart
+++ tests_example/test/pages/golden_test/golden_test_page_test.dart
@@ -57,7 +57,7 @@ void main() {
   });
 
   testGoldens(
-      'Golden Test Page Test - Initial Scenario - Device Builder - Dark Theme',
+      'Golden Test Page Test - Initial Scenario - Device Builder - DarkMode',
       (tester) async {
     //arrange
     deviceBuilder.addScenario(
@@ -77,45 +77,59 @@ void main() {
     await screenMatchesGolden(tester, 'initial_state_screen_dark_mode');
   });
 
-  testGoldens('Golden Test Page Test - Initial Scenario - multiScreenGolden',
-      (tester) async {
-    //act
-    await tester.pumpWidgetBuilder(const GoldenTestPage());
-
-    //assert
-    await multiScreenGolden(tester, 'initial_state_multi_screen');
-  });
-
-  testGoldens('Golden Test Page Test - After Button click Scenario',
+  testGoldens('Golden Test Page Test - Final Scenario - Device Builder',
       (tester) async {
     //arrange
     deviceBuilder.addScenario(
-        name: 'GoldenTestPage after click on ElevatedButton',
+        name: 'GoldenTestPage after click on button',
         widget: const GoldenTestPage(),
-        onCreate: (scenarioKey) async {
+        onCreate: (deviceKey) async {
           final elevatedButton = find.descendant(
-            of: find.byKey(scenarioKey),
+            of: find.byKey(deviceKey),
             matching: find.byKey(
               const Key('elevated-button'),
             ),
           );
+
           await tester.tap(elevatedButton);
           await tester.pump();
 
-          final textAfterClick = find.descendant(
-            of: find.byKey(scenarioKey),
-            matching: find.text(
-              'text after click on button',
-            ),
+          final afterClickText = find.descendant(
+            of: find.byKey(deviceKey),
+            matching: find.text('text after click on button'),
           );
 
-          expect(textAfterClick, findsOneWidget);
+          expect(afterClickText, findsOneWidget);
         });
 
     //act
     await tester.pumpDeviceBuilder(deviceBuilder);
 
     //assert
-    await screenMatchesGolden(tester, 'golden_test_page_after_click_state');
+    await screenMatchesGolden(tester, 'final_state_screen');
+  });
+
+  testGoldens('Golden Test Page - Initial state - multiScreenGolden',
+      (tester) async {
+    await tester.pumpWidgetBuilder(const GoldenTestPage());
+
+    await multiScreenGolden(tester, 'initial_state_screen_multi_screen_golden');
+  });
+
+  testGoldens('Golden Test Page - Final state - multiScreenGolden',
+      (tester) async {
+    await tester.pumpWidgetBuilder(const GoldenTestPage());
+
+    await multiScreenGolden(tester, 'final_state_screen_multi_screen_golden',
+        customPump: (tester) async {
+      final elevatedButton = find.byKey(const Key('elevated-button'));
+
+      await tester.tap(elevatedButton);
+      await tester.pump();
+
+      final afterClickText = find.text('text after click on button');
+
+      expect(afterClickText, findsOneWidget);
+    });
   });
 }
