1dabc575af14fcc8bafa3a06d441f71df644e33c
arthurgiani <arthur@getleslie.com>
2022-05-17 12:46:42 -0300

    tests, goldens and test icon fonts

---
+++ design_system_example/test/src/buttons/main_button_test.dart
@@ -0,0 +1,188 @@
+import 'package:design_system_example/design_system_example.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+void main() {
+  testGoldens('MainButton Enabled/Disabled/Outlined - Theme1', (tester) async {
+    final builder = GoldenBuilder.grid(
+      columns: 2,
+      widthToHeightRatio: 1,
+    );
+
+    builder.addScenario(
+      'Main Button - Enabled',
+      MainButton(
+        isEnabled: true,
+        isBusy: false,
+        label: 'enabled',
+        onPressed: () {},
+      ),
+    );
+
+    builder.addScenario(
+      'Main Button - Disabled',
+      MainButton(
+        isEnabled: false,
+        isBusy: false,
+        label: 'enabled',
+        onPressed: () {},
+      ),
+    );
+
+    builder.addScenario(
+      'Main Button - Outlined',
+      MainButton.outlined(
+        label: 'enabled',
+        icon: const Icon(Icons.exit_to_app),
+        onPressed: () {},
+      ),
+    );
+
+    await tester.pumpWidgetBuilder(
+      builder.build(),
+      wrapper: materialAppWrapper(theme: theme1),
+    );
+
+    await screenMatchesGolden(tester, 'main_button_static_theme1');
+  });
+
+  testGoldens('MainButton Enabled/Disabled/Outlined - Theme2', (tester) async {
+    final builder = GoldenBuilder.grid(
+      columns: 2,
+      widthToHeightRatio: 1,
+    );
+
+    builder.addScenario(
+      'Main Button - Enabled',
+      MainButton(
+        isEnabled: true,
+        isBusy: false,
+        label: 'enabled',
+        onPressed: () {},
+      ),
+    );
+
+    builder.addScenario(
+      'Main Button - Disabled',
+      MainButton(
+        isEnabled: false,
+        isBusy: false,
+        label: 'enabled',
+        onPressed: () {},
+      ),
+    );
+
+    builder.addScenario(
+      'Main Button - Outlined',
+      MainButton.outlined(
+        label: 'enabled',
+        icon: const Icon(Icons.exit_to_app),
+        onPressed: () {},
+      ),
+    );
+
+    await tester.pumpWidgetBuilder(
+      builder.build(),
+      wrapper: materialAppWrapper(theme: theme2),
+    );
+
+    await screenMatchesGolden(tester, 'main_button_static_theme2');
+  });
+
+  testWidgets('Main Button - No packages - Theme1', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: theme1,
+        home: Scaffold(
+          body: Column(
+            children: [
+              MainButton(
+                isEnabled: true,
+                isBusy: true,
+                label: 'Loading',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 32),
+              MainButton(
+                isEnabled: true,
+                isBusy: false,
+                label: 'Loading',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 32),
+              MainButton(
+                isEnabled: false,
+                isBusy: false,
+                label: 'Loading',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 32),
+              MainButton.outlined(
+                icon: const Icon(Icons.ad_units),
+                label: 'Loading',
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    await tester.pump(const Duration(seconds: 2));
+
+    await expectLater(
+      find.byType(MaterialApp),
+      matchesGoldenFile('goldens/main_button_vanilla_theme1.png'),
+    );
+  });
+
+  testWidgets('Main Button - Vanilla - Theme2', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: theme2,
+        home: Scaffold(
+          body: Column(
+            children: [
+              MainButton(
+                isEnabled: true,
+                isBusy: true,
+                label: 'Loading',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 32),
+              MainButton(
+                isEnabled: true,
+                isBusy: false,
+                label: 'Loading',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 32),
+              MainButton(
+                isEnabled: false,
+                isBusy: false,
+                label: 'Loading',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 32),
+              MainButton.outlined(
+                icon: const Icon(Icons.ad_units),
+                label: 'Loading',
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    await tester.pump(const Duration(seconds: 2));
+
+    await expectLater(
+      find.byType(MaterialApp),
+      matchesGoldenFile('goldens/main_button_vanilla_theme2.png'),
+    );
+  });
+}
---
+++ design_system_example/test/src/textfields/main_text_field_test.dart
@@ -0,0 +1,133 @@
+import 'package:design_system_example/design_system_example.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+/// [InputDecorationTheme] is the same for both [theme1] and [theme2].
+///
+/// Therefore, only one scenario is necessary to test.
+
+void main() {
+  testGoldens('MainTextField - All Scenarios - Theme1', (tester) async {
+    final builder = GoldenBuilder.grid(
+      columns: 2,
+      widthToHeightRatio: 1,
+    );
+
+    builder.addScenario(
+      'With leading / trailling',
+      MainTextField(
+        textController: TextEditingController(),
+        hintText: 'With leading / trailling',
+        leading: const Icon(Icons.clear),
+        trailling: const Icon(Icons.clear),
+        onTraillingPressed: () {},
+      ),
+    );
+
+    builder.addScenario(
+      'With leading',
+      MainTextField(
+        textController: TextEditingController(),
+        hintText: 'With leading',
+        leading: const Icon(Icons.clear),
+        onTraillingPressed: () {},
+      ),
+    );
+
+    builder.addScenario(
+      'With trailling',
+      MainTextField(
+        textController: TextEditingController(),
+        hintText: 'With trailling',
+        trailling: const Icon(Icons.clear),
+        onTraillingPressed: () {},
+      ),
+    );
+
+    builder.addScenario(
+      'No leading and trailling',
+      MainTextField(
+        textController: TextEditingController(),
+        hintText: 'No leading and trailling',
+        onTraillingPressed: () {},
+      ),
+    );
+
+    builder.addScenario(
+      'Obscure',
+      MainTextField(
+        isObscure: true,
+        textController: TextEditingController(text: 'obscure text'),
+        hintText: 'With leading / trailling',
+        leading: const Icon(Icons.clear),
+        trailling: const Icon(Icons.clear),
+        onTraillingPressed: () {},
+      ),
+    );
+
+    await tester.pumpWidgetBuilder(
+      builder.build(),
+      wrapper: materialAppWrapper(theme: theme1),
+    );
+
+    await screenMatchesGolden(tester, 'main_text_field_theme1');
+  });
+
+  testWidgets('Main Text - Vanilla - Theme1', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: theme1,
+        home: Scaffold(
+          body: Column(
+            children: [
+              MainTextField(
+                textController: TextEditingController(),
+                hintText: 'No leading / trailling',
+              ),
+              const SizedBox(height: 16),
+              MainTextField(
+                textController: TextEditingController(),
+                hintText: 'Leading',
+                leading: const Icon(Icons.clear),
+              ),
+              const SizedBox(height: 16),
+              MainTextField(
+                textController: TextEditingController(),
+                hintText: 'Trailling',
+                trailling: const Icon(Icons.clear),
+                onTraillingPressed: () {
+                  debugPrint('Pressed');
+                },
+              ),
+              const SizedBox(height: 16),
+              MainTextField(
+                textController: TextEditingController(),
+                hintText: 'With leading / trailling',
+                leading: const Icon(Icons.clear),
+                trailling: const Icon(Icons.clear),
+                onTraillingPressed: () {},
+              ),
+              const SizedBox(height: 16),
+              MainTextField(
+                textController: TextEditingController(text: 'obscure text'),
+                hintText: 'Obscure',
+                isObscure: true,
+                trailling: const Icon(Icons.clear),
+                onTraillingPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    await tester.pump(const Duration(seconds: 2));
+
+    await expectLater(
+      find.byType(MaterialApp),
+      matchesGoldenFile('goldens/main_text_field_vanilla_theme1.png'),
+    );
+  });
+}
