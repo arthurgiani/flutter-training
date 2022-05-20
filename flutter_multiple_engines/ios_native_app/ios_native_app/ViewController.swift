ed29cba6f54758153feae162a5dae853dbe0fb2d
arthurgiani <arthur@getleslie.com>
2022-05-20 11:59:44 -0300

    ios native app for flutter multiple engine created

---
+++ flutter_multiple_engines/ios_native_app/ios_native_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_native_app
+//
+//  Created by Arthur Giani Alves Pereira on 20/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
