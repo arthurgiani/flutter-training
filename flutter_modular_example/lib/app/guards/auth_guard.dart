8cc9796da9536366a016e47065491bde8b21c9f3
arthurgiani <arthur@getleslie.com>
2022-05-31 09:59:16 -0300

    doc and formatting

--- flutter_modular_example/lib/app/guards/auth_guard.dart
+++ flutter_modular_example/lib/app/guards/auth_guard.dart
@@ -1,13 +1,13 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
-/// Use a RouteGuard to verify a certain condition before redirect a user
+/// Use a [RouteGuard] to verify a certain condition before redirect a user
 /// to a specific page.
 
 class AuthGuard extends RouteGuard {
   AuthGuard() : super(redirectTo: '/counter/');
 
   @override
-  Future<bool> canActivate(String path, ModularRoute router) {
+  Future<bool> canActivate(String path, ModularRoute route) {
     return Future.value(false);
   }
 }
--- flutter_modular_example/lib/app/modules/counter/value_notifiers/counter_notifier.dart
+++ flutter_modular_example/lib/app/modules/counter/value_notifiers/counter_notifier.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class CounterNotifier extends ValueNotifier<int> {
   CounterNotifier() : super(0);
-
   void increment() {
     value++;
   }
